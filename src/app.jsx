f36a0ce39d110307673c162f090bfc759bd9ab4d
Andres Cachero <acachero-c@g2llc.com.ar>
2016-03-31 08:58:30 -0300

    Excercise from module 1

--- src/app.jsx
+++ src/app.jsx
@@ -2,20 +2,42 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 var Button = React.createClass({
+    clickHandler() {
+        this.props.localClickHandler(this.props.increment)
+    },
+    render() {
+        return <button onClick={this.clickHandler}>+{this.props.increment}</button>;
+    }
+});
+
+var Result = React.createClass({
+    render() {
+        return <div>{this.props.localCounter}</div>;
+    }
+});
+
+var Main = React.createClass({
     getInitialState() {
         return {
             counter: 0
         };
     },
-    clickHandler() {
-        this.setState({counter: this.state.counter+1})
+    clickHandler(increment) {
+        this.setState({counter: this.state.counter + increment})
     },
     render() {
-        return <button onClick={this.clickHandler}>{this.state.counter}</button>;
+        return (
+            <div>
+                <Button localClickHandler={this.clickHandler} increment={1} />
+                <Button localClickHandler={this.clickHandler} increment={5} />
+                <Button localClickHandler={this.clickHandler} increment={10} />
+                <Button localClickHandler={this.clickHandler} increment={100} />
+                <Result localCounter={this.state.counter} />
+            </div>
+        );
     }
-});
-
-ReactDOM.render(<Button />, document.getElementById("main"));
+})
+ReactDOM.render(<Main />, document.getElementById("main"));
 
 
 
