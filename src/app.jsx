8e64e4e16772e41b553a9f1a6a11a0a121888841
Andres Cachero <acachero-c@g2llc.com.ar>
2016-04-01 15:48:29 -0300

    List github users

--- src/app.jsx
+++ src/app.jsx
@@ -1,21 +1,68 @@
+var $ = require('jquery');
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var Button = React.createClass({
+var Card = React.createClass({
     getInitialState() {
-        return {
-            counter: 0
-        };
+        return {};
     },
-    clickHandler() {
-        this.setState({counter: this.state.counter+1})
+    componentDidMount() {
+        var component = this;
+        $.get('https://api.github.com/users/' + component.props.username, function (data) {
+            component.setState(data);
+        });
     },
     render() {
-        return <button onClick={this.clickHandler}>{this.state.counter}</button>;
+        return (
+            <div>
+                <img src={this.state.avatar_url} width="80" />
+                <h3>{this.state.name}</h3>
+                <hr />
+            </div>
+        );
     }
 });
 
-ReactDOM.render(<Button />, document.getElementById("main"));
+var Form = React.createClass({
+    addUsername(event) {
+        event.preventDefault();
+        username = ReactDOM.findDOMNode(this.refs.username);
+        this.props.addUsername(username.value);
+        username.value = '';
+    },
+    render() {
+        return (
+            <form onSubmit={this.addUsername} >
+                <input placeholder="github username" ref="username" />
+                <input type="submit" text="Add" />
+            </form>
+        );
+    }
+});
+
+var Main = React.createClass({
+    getInitialState() {
+        return {
+            usernames: []
+        };
+    },
+    addUsername(username) {
+        this.setState({username: this.state.usernames.push(username)});
+    },
+    render() {
+        var cards = this.state.usernames.map(function (username) {
+            return (<Card key={username} username={username} />);
+        });
+        return (
+            <div>
+                <Form addUsername={this.addUsername} />
+                {cards}
+            </div>
+        );
+    }
+});
+
+ReactDOM.render(<Main />, document.getElementById("main"));
 
 
 
